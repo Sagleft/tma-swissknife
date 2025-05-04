3158661e97b13e88a5414bec79696480590999d1
Sagleft <sagleft@mfc.click>
2025-05-04 16:25:28 +0300

    Feat/handler packer (#1)
    * add handler base
    
    * update constructor

---
+++ handler/handler.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"fmt"
+
+	"github.com/Sagleft/tma-swissknife/rest"
+)
+
+type Handler interface {
+	Init(methods map[string]HandlerFunc)
+
+	HandleRequest(rest.Request) (rest.Message, error)
+}
+
+type handler struct {
+	methods map[string]HandlerFunc
+}
+
+type HandlerFunc func(data any) (rest.Message, error)
+
+func New() *handler {
+	return &handler{}
+}
+
+func (h *handler) Init(methods map[string]HandlerFunc) {
+	h.methods = methods
+}
+
+func (h *handler) HandleRequest(req rest.Request) (rest.Message, error) {
+	method, ok := h.methods[req.Method]
+	if !ok {
+		return rest.Message{}, fmt.Errorf("method %q not found", req.Method)
+	}
+	return method(req.Data)
+}
