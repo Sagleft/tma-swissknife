bc71138b16949be9c5d8cbd0d9a471af931083a9
Sagleft <sagleft@mfc.click>
2025-11-14 21:03:22 +0300

    add version system (#5)
    * add version system
    
    * change hash func

---
+++ helpers/hash.go
@@ -0,0 +1,25 @@
+package helpers
+
+import (
+	"crypto/md5"
+	"encoding/base32"
+	"encoding/binary"
+	"encoding/hex"
+
+	"github.com/cespare/xxhash/v2"
+)
+
+// MD5 - calc MD5 checksum
+func MD5(val []byte) string {
+	hash := md5.Sum(val)
+	return hex.EncodeToString(hash[:])
+}
+
+func XxHash64Base32(data []byte) string {
+	hash := xxhash.Sum64(data)
+	// Преобразуем uint64 в байты в порядке little-endian
+	buf := make([]byte, 8)
+	binary.LittleEndian.PutUint64(buf, hash)
+	// Кодируем байты в base32 без padding
+	return base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(buf)
+}
